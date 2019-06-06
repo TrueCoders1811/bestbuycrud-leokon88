3c19a98c162417ac160153d0334a4703d9a4fcbf
YShin <yookyung.shin@gmail.com>
2019-06-06 18:04:45 -0500

    Update aand Delete statement added

--- BestBuyCRUD/SalesRepo.cs
+++ BestBuyCRUD/SalesRepo.cs
@@ -30,17 +30,83 @@ namespace BestBuyCRUD
                 List<Sales> sales = new List<Sales>();
                 while(reader.Read())
                 {
-
                     Sales s = new Sales();
                     s.SalesId = (int)reader["SID"];
                     s.ProductId = (int)reader["PID"];
                     s.Quantity = (int)reader["Q"];
-                    s.Price = (int)reader["Price"];
+                    s.Price = (decimal)reader["Price"];
                     s.Date = Convert.ToDateTime(reader["Date"]);
                     s.EmployeeId = (int)reader["EID"];
                 }
+                return sales;
             }
         }
+
+        public void CreateSales(Sales salesToCreate)
+        {
+            MySqlConnection connect = new MySqlConnection(connectionString);
+
+            using (connect)
+            {
+                connect.Open();
+
+                MySqlCommand cmd = connect.CreateCommand();
+                cmd.CommandText = "INSERT INTO Sales(SalesID, ProductID, Quantity, Price,Date,EmployeesID)"
+                    + "(@SID,@PID,@Q, @Price, @Date, @EID)";
+                cmd.Parameters.AddWithValue("SID", salesToCreate);
+                cmd.Parameters.AddWithValue("PID", salesToCreate);
+                cmd.Parameters.AddWithValue("Q", salesToCreate);
+                cmd.Parameters.AddWithValue("Price", salesToCreate);
+                cmd.Parameters.AddWithValue("Date", salesToCreate);
+                cmd.Parameters.AddWithValue("EID", salesToCreate);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void UpdateSales(Sales salesToUpate)
+        {
+            MySqlConnection connect = new MySqlConnection(connectionString);
+
+            using (connect)
+            {
+                connect.Open();
+                MySqlCommand cmd = connect.CreateCommand();
+
+                cmd.CommandText = "Update Sales SET SalesId =@SID, ProductId =@PID, Quantity=@Q"
+                    + "Price=@price, Date=@date, EmployeeID=@EID";
+                cmd.Parameters.AddWithValue("SID", salesToUpate.SalesId);
+                cmd.Parameters.AddWithValue("PID", salesToUpdate.ProductId);
+                cmd.Parameters.AddWithValue("Q", salesToUpdate.Quantity);
+                cmd.Parameters.AddWithValue("Price", salesToUpdate.Price);
+                cmd.Parameters.AddWithValue("Date", salesToUpdate.Date);
+                cmd.Parameters.AddWithValue("EID", salesToUpdate.EmployeeId);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void DeleteSales( Sales salesToDelete)
+        {
+            MySqlConnection connect = new MySqlConnection(connectionString);
+            using (connect)
+            {
+                connect.Open();
+
+                MySqlCommand cmd = connect.CreateCommand();
+                cmd.CommandText = "DELECT FROM Sales ??";
+                cmd.Parameters.AddWithValue("SID", salesToDelete.SalesId);
+                cmd.Parameters.AddWithValue("PID", salesToDelete.ProductId);
+                cmd.Parameters.AddWithValue("Price", salesToDelete.Price);
+                cmd.Parameters.AddWithValue("Q", salesToDelete.Quantity);
+                cmd.Parameters.AddWithValue("EID", salesToDelete.EmployeeId);
+                cmd.Parameters.AddWithValue("Date", salesToDelete.Date);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+
     }
 
 }
