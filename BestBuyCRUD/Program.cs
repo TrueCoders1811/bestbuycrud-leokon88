eca47901cba3dca4a268b3ab35dbcf057b3f4b90
YShin <yookyung.shin@gmail.com>
2019-06-16 23:22:13 -0500

    Employee table read operation modified

--- BestBuyCRUD/Program.cs
+++ BestBuyCRUD/Program.cs
@@ -25,25 +25,24 @@ namespace BestBuyCRUD
             bool isTrue = false;
          
            
-            Console.Clear();
+            //Console.Clear();
             while (!isTrue)
             {
 
                 Console.WriteLine("Select from the Menu:");
                 Console.WriteLine("1) Create a new employee");
-                Console.WriteLine("2) Update an employee");
-                Console.WriteLine("3) Delete an  employee");
-                Console.WriteLine("4) Display a employee list");
+                Console.WriteLine("2) Update last employee");
+                Console.WriteLine("3) Delete last  employee");
+                Console.WriteLine("4) Display an complete employee list");
                 Console.WriteLine("5) Search for an employee");
                 Console.WriteLine("6) Exit");
 
                 int userInput = int.Parse(Console.ReadLine());
-                //Console.Clear();
-                // int employeeCount;
-                // int lastEmployeeId;
+                Console.Clear();
+            
                List<Employees> listOfEmployees = er.GetEmployees();
                int employeeCount = listOfEmployees.Count - 1; // getting last index of employee 
-               int lastEmployeeId = listOfEmployees[employeeCount].EmployeeId;
+               int lastEmployeeId = listOfEmployees[employeeCount].EmployeeId;// retrieving employeeId from last employee in the listOfEmployees list)
 
                 switch (userInput)
                 {
@@ -65,29 +64,34 @@ namespace BestBuyCRUD
 
                         Employees newEmployee = new Employees(firstName, middleName, lastName, email, number, title, today);// constructor created to create a new employee
                         er.CreateEmployees(newEmployee);
-                       // List<Employees> listOfEmployees = er.GetEmployees();
-                       // int employeeCount = listOfEmployees.Count-1; // getting last index of employee 
-                       // int lastEmployeeId = listOfEmployees[employeeCount].EmployeeId; //retrieving employeeId from last employee in the listOfEmployees list) 
                         Console.WriteLine("New employee created.");
                         break;
 
                     case 2:
-                        //Update Employee info
+                        //Update last Employee info
                         er.UpdateEmployees(lastEmployeeId,"Thomas" , "S", "Lim");
                         Console.WriteLine($"Employee {lastEmployeeId} updated.");
                         break;
                     case 3:
-                        Console.WriteLine($"Are you sure you want to delect Employee  ?");
-                   
-                      // er.DeleteEmployees(lastEmployeeId);
-
-                        Console.WriteLine("Employee #?? deleted");
+                        //Delete last Employee info
+                        Console.WriteLine($"Are you sure you want to delete Employee# {lastEmployeeId} ?");
+                        er.DeleteEmployees(lastEmployeeId);
+                        Console.WriteLine($"Employee# {lastEmployeeId} deleted");
                         break;
                     case 4:
-                        Console.WriteLine("Employee List");
+                        Console.WriteLine("Employee List:");
+                        foreach (Employees emp in listOfEmployees)
+                        {
+                            Console.WriteLine($"ID: {emp.EmployeeId}   FirstName: {emp.FirstName}  MiddleInitial: {emp.MiddleInitial}"
+                                + $"LastName:{emp.LastName}  Email: {emp.EmailAddress}   Phone:{emp.PhoneNumber}  Title: {emp.Title}  Date of Birth : {emp.DateOfBirth}");
+                        }
+                        Console.Read();
                         break;
                     case 5:
                         Console.WriteLine("Enter an employee # to search");
+                        int empSearch =int.Parse(Console.ReadLine());
+                        Console.WriteLine($"ID: {emp.EmployeeId}   FirstName: {emp.FirstName}  MiddleInitial: {emp.MiddleInitial}"
+                                + $"LastName:{emp.LastName}  Email: {emp.EmailAddress} ");
                         break;
                     case 6:
                         Console.WriteLine("Exiting Menu ");
